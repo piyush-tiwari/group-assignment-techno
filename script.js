1a1298be851146a14279dcc789c0a1ed28b4d449
piyush-tiwari <t.piyush@iitg.ac.in>
2020-03-10 23:41:03 +0530

    Preliminary algo

---
+++ script.js
@@ -0,0 +1,33 @@
+function awardbadge(i){
+	let badge=document.querySelector(`ul li:nth-of-type(${i+1})`);
+	badge.style.backgroundColor="green";
+}
+let data=[0,1999,0,1000,0,0,5,0,0,0];
+// pehle paanch categories, sixth is rank, warrior attempt, warrior correct, total attempt, total correct
+const max=2000;
+const totalque = 22;
+const totalwar = 5;
+for(i=0;i<5;i++)
+{
+	if(data[i]>0.85*2000)
+	{
+	awardbadge(i);
+	}
+}
+
+if(data[5]<101)
+{
+	awardbadge(5);
+}
+if(data[6]===totalwar) //braveheart
+{
+	awardbadge(6);
+}
+if(data[7]===totalwar) //god!!!
+{
+	awardbadge(7);
+}
+if((data[9]/data[8]*100)>=85)
+{
+awardbadge(8)
+}
